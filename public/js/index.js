10eaf82f9a67908a6e9699d39508c2a35a510a38
Jennifer Earl <jearl@braeburn.local>
2018-07-12 20:52:31 -0700

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Bar Bazz',
-        text: 'Zzzzz'
-    });
 });
 socket.on('disconnect', function() {
     console.log('Disconnected.');
--- server/server.js
+++ server/server.js
@@ -20,21 +20,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey whats up?',
-        createAt: '2018-07-10'
-    });
-
-
     socket.on('createMessage', (newMsg) => {
         console.log('createMessage', newMsg);
-    });
-
-    socket.emit('newMessage', {
-        from: 'Joe Bob',
-        text: 'Hey whats up?',
-        createdAt: new Date().toDateString()
+        io.emit('newMessage', {
+            from: newMsg.from,
+            text: newMsg.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
