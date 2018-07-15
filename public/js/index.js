d041354064c0aba23524421048b1776dc0684198
Jennifer Earl <jearl@braeburn.local>
2018-07-15 09:00:57 -0700

    added CSS and tweaked Send location button

--- public/js/index.js
+++ public/js/index.js
@@ -34,11 +34,12 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(evt) {
     evt.preventDefault();
 
+    var msgTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: msgTextbox.val()
     }, function () {
-
+        msgTextbox.val('');
     });
 });
 
@@ -48,13 +49,17 @@ locationButton.on('click', function() {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         console.log(position);
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
